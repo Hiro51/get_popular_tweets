d06895af32e247ad372be657ecd05c7d6b10385e
hiro <h.tsujino0714@gmail.com>
2014-08-17 14:32:11 -0700

    revised get_list_tweets

--- get_list_tweets.rb
+++ get_list_tweets.rb
@@ -1,19 +1,52 @@
 require 'json'
 require 'oauth'
 
-def setup(consumer_key, consumer_secret, access_token, access_token_secret)
-	consumer = OAuth::Consumer.new(
-	  consumer_key, 
-	  consumer_secret, 
-	  site:'https://api.twitter.com/'
-	)
-	endpoint = OAuth::AccessToken.new(consumer, access_token, access_token_secret)
+class GetPopTwList
+	class << self
+		def setup(consumer_key: '', consumer_secret: '', access_token: '', access_token_secret: '')
+			@endpoint = OAuth::AccessToken.new(
+				consumer(consumer_key, consumer_secret),
+				access_token,
+				access_token_secret
+			)
+		end
+
+		def set_opt(favorite_count: 5, retweet_count: 10, is_logical_and: true )
+		  @opt = {}
+		  @opt[:favorite_count] = favorite_count
+		  @opt[:retweet_count] = retweet_count
+		  @opt[:is_logical_and] = is_logical_and
+		end
+
+		def get_tweets(slug_name: '', screen_name: '')
+			response = @endpoint.request(:get, "https://api.twitter.com/1.1/lists/statuses.json?slug=#{slug_name}&owner_screen_name=#{screen_name}")
+			tweets = JSON.parse(response.body).map do |result| 
+				{
+					screen_name: result["user"]["screen_name"],
+					contents: result["text"],
+					favorite_count: result["favorite_count"],
+					retweet_count: result["retweet_count"]
+				}
+			end
+			tweets.select { |tweet| 
+				is_more_fav = tweet[:favorite_count] > @opt[:favorite_count]
+				is_more_rt = tweet[:retweet_count] > @opt[:retweet_count]
+				if @opt[:is_logical_and]
+					is_more_fav && is_more_rt 
+				else 
+					is_more_fav || is_more_rt
+				end
+				}
+		end
+
+		private
+			def consumer(consumer_key, consumer_secret)
+			  OAuth::Consumer.new(
+				  consumer_key, 
+				  consumer_secret, 
+				  site:'https://api.twitter.com/'
+				)
+		  end
+	end
 end
 
-def show_tweets(slug_name, screen_name)
-	response = endpoint.request(:get, "https://api.twitter.com/1.1/lists/statuses.json?slug=#{slug_name}&owner_screen_name=#{screen_name}")
-	result = JSON.parse(response.body)
-	tweet = result.map { |ary| [ary["user"]["screen_name"], ary["text"], ary["favorite_count"],ary["retweet_count"]] }
-	chosen_tweet = tweet.select { |item| item[2] > 5 || item[3] >10 }
-	puts chosen_tweet
-end
